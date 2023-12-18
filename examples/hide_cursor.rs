729de3d71f3c2c694664c7ad827a73c385e0783c
Jeff Schwab <jeff@wieldy.ai>
2023-12-17 23:41:18 -0500

    Simplify examples
    Run it like this to see the bus error:
    
    ```sh
    $ cargo run --example hide_cursor > out 2>&1 |rg -c Hiding
    82
    [1]    58931 bus error  cargo run --example hide_cursor > out 2>&1 |
           58932 done       rg -c Hiding
    ```

--- examples/hide_cursor.rs
+++ examples/hide_cursor.rs
@@ -1,37 +1,18 @@
 #![allow(clippy::single_match)]
 
 use simple_logger::SimpleLogger;
-use winit::{
-    event::{Event, WindowEvent},
-    event_loop::EventLoop,
-    window::WindowBuilder,
-};
-
-#[path = "util/fill.rs"]
-mod fill;
+use winit::{event_loop::EventLoop, window::WindowBuilder};
 
 fn main() -> Result<(), impl std::error::Error> {
     SimpleLogger::new().init().unwrap();
     let event_loop = EventLoop::new().unwrap();
 
     let window = WindowBuilder::new().build(&event_loop).unwrap();
-    window.set_title("Bus error on key press");
+    window.set_title("Hide cursor");
 
-    event_loop.run(move |event, elwt| {
-        if let Event::WindowEvent { event, .. } = event {
-            match event {
-                WindowEvent::KeyboardInput { event, .. } if event.state.is_pressed() => {
-                    println!("Hiding cursor.");
-                    window.set_cursor_visible(false);
-                }
-                WindowEvent::RedrawRequested => {
-                    fill::fill_window(&window);
-                }
-                WindowEvent::CloseRequested => {
-                    elwt.exit();
-                }
-                _ => (),
-            }
-        }
+    event_loop.run(move |_event, _elwt| {
+        eprint!("Hiding cursor...");
+        window.set_cursor_visible(false);
+        eprintln!("OK");
     })
 }
